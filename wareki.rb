315f9368b83bddcddfed2adbb6218a3b54ba729f
ebc-2in2crc <y015h1n80@gmail.com>
2018-05-26 17:55:26 +0900

    Add: version

--- wareki.rb
+++ wareki.rb
@@ -6,6 +6,7 @@ class Wareki < Formula
   homepage "https://github.com/ebc-2in2crc/homebrew-wareki"
   url "https://github.com/ebc-2in2crc/wareki/releases/download/v0.9.0/wareki_darwin_amd64.zip"
   sha256 "8beff4490d5b02455d900defb2524a90ba394775d815ed5d091ac3641a06e59f"
+  version "0.9.0"
 
   def install
     bin.install "wareki"
