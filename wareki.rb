41b882fd1b8496925c13d1e5467f9a65eac5e304
ebc-2in2crc <y015h1n80@gmail.com>
2019-07-10 23:54:10 +0900

    Change: v1.0.0

--- wareki.rb
+++ wareki.rb
@@ -1,9 +1,9 @@
 class Wareki < Formula
   desc "西暦を和暦に変換"
   homepage "https://github.com/ebc-2in2crc/homebrew-wareki"
-  url "https://github.com/ebc-2in2crc/wareki/releases/download/v0.10.0/wareki_darwin_amd64.zip"
-  sha256 "7b082a8e4844484a468a545238c597bb89cc43b8f37e60742a53b3605c0d7cbb"
-  version "0.10.0"
+  url "https://github.com/ebc-2in2crc/wareki/releases/download/v1.0.0/wareki_darwin_amd64.zip"
+  sha256 "433e2ae0bdff27178648694bc09e8ea6ae40b4c3bc5af5da0c8e5db9952e6f84"
+  version "1.0.0"
 
   def install
     bin.install "wareki"
