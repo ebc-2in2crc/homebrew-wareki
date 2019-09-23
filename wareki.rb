f82e9495b2d9ed798ffe6713066768bbc9f6dc48
ebc-2in2crc <y015h1n80@gmail.com>
2019-09-24 00:55:03 +0900

    Change: JA => EN

--- wareki.rb
+++ wareki.rb
@@ -1,5 +1,5 @@
 class Wareki < Formula
-  desc "西暦を和暦に変換"
+  desc "Converts between Japanese calendar and western calendar"
   homepage "https://github.com/ebc-2in2crc/homebrew-wareki"
   url "https://github.com/ebc-2in2crc/wareki/releases/download/v1.0.0/wareki_darwin_amd64.zip"
   sha256 "433e2ae0bdff27178648694bc09e8ea6ae40b4c3bc5af5da0c8e5db9952e6f84"
