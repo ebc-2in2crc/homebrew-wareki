130010a0d860840a103f8ca654b36cc4bc43c7a3
ebc-2in2crc <y015h1n80@gmail.com>
2020-02-20 22:40:03 +0900

    Fix: brew audit warning

--- wareki.rb
+++ wareki.rb
@@ -2,8 +2,8 @@ class Wareki < Formula
   desc "Converts between Japanese calendar and western calendar"
   homepage "https://github.com/ebc-2in2crc/homebrew-wareki"
   url "https://github.com/ebc-2in2crc/wareki/releases/download/v1.0.1/wareki_darwin_amd64.zip"
-  sha256 "b86a76e1023bba25a16638ef4ba46d8d178bc3853ee9ed9ef640bc1eba78a164"
   version "1.0.1"
+  sha256 "b86a76e1023bba25a16638ef4ba46d8d178bc3853ee9ed9ef640bc1eba78a164"
 
   def install
     bin.install "wareki"
