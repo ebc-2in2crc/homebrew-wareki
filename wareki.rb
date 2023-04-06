db380a3fdd7c514dae511fafaf86a30001ff94de
ebc-2in2crc <y015h1n80@gmail.com>
2023-04-06 20:44:21 +0900

    Change: v1.2.1

--- wareki.rb
+++ wareki.rb
@@ -1,9 +1,9 @@
 class Wareki < Formula
   desc "Converts between Japanese calendar and western calendar"
   homepage "https://github.com/ebc-2in2crc/homebrew-wareki"
-  url "https://github.com/ebc-2in2crc/wareki/releases/download/v1.2.0/wareki_darwin_amd64.zip"
-  version "1.2.0"
-  sha256 "4ddbd4a172b7c3898299fd7a112f91106ae07c0976f3e781b66de31ee6c7a9fe"
+  url "https://github.com/ebc-2in2crc/wareki/releases/download/v1.2.1/wareki_darwin_amd64.zip"
+  version "1.2.1"
+  sha256 "d47674ed036ccce7e9637f5ab932b4a4ea5d221ce18584f6b0949c32a5515afd"
 
   def install
     bin.install "wareki"
